f3564e7aaf608fe426d11c9a069332c56b3800fc
Przemysław Kowalczyk <przemek.t.kowalczyk@gmail.com>
2020-11-11 17:35:33 +0100

    Add new general styles for responsive-grid.scss

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,12 @@ document.querySelector('.navigation-menu').addEventListener('click', function(ev
   console.log('got it?', toggleMenu);
 });
 
+var fixedTopNav = document.querySelector('.fixed-top');
+window.addEventListener('scroll', function(){
+  fixedTopNav.classList.toggle('top-slider', window.scrollY > 600);
+});
+
 window.addEventListener('scroll', function(){
   var navBar = document.querySelector('.navigation-menu');
-  navBar.classList.toggle('show', window.scrollY > 0);
+  navBar.classList.toggle('show', window.scrollY > 600);
 });
