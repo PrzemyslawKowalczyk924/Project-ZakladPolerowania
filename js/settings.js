7b021272d4f38bba04416c4d3466b784630dc967
unknown <kowalczyk.przemek@wp.pl>
2021-08-25 21:10:31 +0200

    Remove dist from gitignore

---
+++ js/settings.js
@@ -0,0 +1,24 @@
+/* eslint-disable indent */
+
+export const select = {
+    containerOf: {
+        pages: '#pages',
+    },
+    nav: {
+        links: '.main-nav a:not(.icon)',
+      },
+    navigation: {
+        top: '.fixed-top',
+    }  
+};
+
+export const classNames = {
+    pages: {
+        active: 'active',
+      },
+    nav: {
+    active: 'active',
+    hide: 'hide',
+    },
+    splash: 'splash',
+};
