225a28d3b3d13a5c0968ee6cf08f5deb10cfab18
Komputer <kowalczyk.przemek@wp.pl>
2023-02-28 04:50:48 +0100

    Remove useless js functions for translating images & and some SEO attribiutes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,28 +1,5 @@
 'use strict';
 
-/* const mainSideHamburger = document.querySelector('.main-side-hamburger');
-mainSideHamburger.addEventListener('click', function(){
-  navBar.classList.toggle('show');
-}); */
-
-/* const navBar = document.querySelector('.navigation-menu');
-window.addEventListener('scroll', function(){
-  
-  if (window.location.hash == '' || window.location.hash == '#/main'){
-    navBar.classList.toggle('show', (window.innerHeight + window.scrollY) >= document.body.offsetHeight);  
-  } else {
-    navBar.classList.add('show');
-    console.log('zadziałał show z linijki 11');
-  }
-}); */
-
-const actualLink = window.location.href;
-
-const hamburgerNav = document.querySelector('.hamburger');
-hamburgerNav.addEventListener('click', function(){
-  //navBar.classList.toggle('show'); z jakiegoś powodu tutaj mamm nieistniejący navBar
-});
-
 window.addEventListener('hashchange', function(){
   const fixedTopNav = document.querySelector('.fixed-top');
   //console.log('top', fixedTopNav);
@@ -36,60 +13,23 @@ window.addEventListener('hashchange', function(){
   }
 });
 
-//icon-animation
+//button gallery
 
+const mainDiv = document.querySelector('#main');
+console.log(mainDiv);
+const buttonGallery = mainDiv.querySelectorAll('.btn-one');
 const galleryBar = document.getElementById('gallery');
-const aboutBar = document.getElementById('about');
 
-const workImage = document.querySelectorAll('.img-work');
-const workImgArr = Array.from(workImage);
-
-const photoIcon = document.querySelectorAll('.old-camera');
-const photoIconArr = Array.from(photoIcon);
-
-function ImgAndIconListener(photo, icon) {
-
- for(let index = 0; index < photo.length; index++) {
-
-  photo[index].addEventListener('mouseenter', () => {
-    icon[index].style.transform = 'translate(-28px, -20px)';
-    icon[index].style.opacity = '1';
-  
-    photo[index].style.transform = 'translate(-28px, -20px)';
-    photo[index].style.transition = '1s';
+buttonGallery.forEach(button => {
+  button.addEventListener('click', function(event) {
+    const clickedElement = this;
+    event.preventDefault();
+    console.log(clickedElement);
+    console.log(button);
+    const id = clickedElement.getAttribute('href').replace('#', '');
+    console.log(id);
+    window.location.hash = '#/' + id;
+    mainDiv.classList.remove('active');
+    galleryBar.classList.add('active', 'sub-gallery');
   });
-
-  photo[index].addEventListener('mouseleave', () => {
-    icon[index].style.transform = 'translate(28px, 20px)';
-    photo[index].style.transform = 'translate(0, 0)';
-
-  if(icon[index].style.transform == 'translate(28px, 20px)') {
-    icon[index].style.opacity = '0';
-  }});
-
-  icon[index].addEventListener('mouseenter', event => {
-    if(event.target == icon[index]) {
-      photo[index].style.transform = 'translate(-28px, -20px)';
-      icon[index].style.transform = 'translate(-28px, -20px)';
-      icon[index].style.opacity = '1';
-      icon[index].addEventListener('click', () => {
-        window.location.hash = '#/gallery';
-        aboutBar.classList.remove('active');
-        galleryBar.classList.add('active', 'sub-gallery');
-        photo[index].style.transform = 'translate(0, 0)';
-        icon[index].style.transform = 'translate(28px, 20px)';
-        icon[index].style.opacity = '0';
-      });
-    }
-  });
- }
-}
-
-ImgAndIconListener(workImgArr, photoIconArr);
-
-console.log(window.location.hash);
-
-console.log(galleryBar);
-
-const screenWidth = window.screen.width;
-console.log(screenWidth);
+});
