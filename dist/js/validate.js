3674a8575c09a0482e435480e1ec0c51d963bebe
Komputer <kowalczyk.przemek@wp.pl>
2023-03-21 01:37:50 +0100

    Huge update after vite package upload for rerender this project. Also added server for handling form

--- dist/js/validate.js
+++ dist/js/validate.js
@@ -1,12 +1,25 @@
-/* eslint-disable indent */
-document.querySelector('form').addEventListener('submit', function(event) {
-    var isFormValidate = true;
+const form = document.querySelector('form');
+const emailInput = document.querySelector('#email');
+const emailError = document.getElementById('email-error');
+
+const emailRegex = new RegExp("^[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,8}\\.)+[a-zA-Z]{2,6}$", "i");
+
+emailInput.addEventListener('change', (event) => {
   
-    var emailAddressInput = event.target.querySelector('input[name="email_address"]');
-    if(emailAddressInput.value.indexOf('@') < 0) {
-      isFormValidate = false;
-      emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+  let result = emailRegex.test(event.target.value);
+  console.log(event.target.value.length);
+  if (result == false) {
+    emailError.textContent = 'Wprowadź poprawny adres e-mail';
+    emailError.style.color = 'red';
+    emailInput.style.borderColor = 'red';
+  } else {
+    if (result == true) {
+      emailError.textContent = '';
+      emailInput.style.borderColor = 'rgb(133, 133, 133)';
     }
-  
-    return !isFormValidate ? event.preventDefault() : true;
+  }
+  if (event.target.value.length == 0) {
+    emailError.textContent = '';
+    emailInput.style.borderColor = 'rgb(133, 133, 133)';
+  }
 });
--- server.js
+++ server.js
@@ -5,44 +5,97 @@ const nodemailer = require('nodemailer');
 
 const PORT = process.env.PORT || 5000;
 
-//Midleware
+//Middleware
 
 app.use(cors({origin: 'http://localhost:3000'}));
 app.use(express.static('src'));
 app.use(express.json());
 
+//RateLimitation func
+
+//Define map to keep track of the number of requests per IP address
+const requestLCount = new Map();
+const bannedIPs = new Map();
+
+//Define the rate limit in requests per minute
+const rateLimit = 5; // 10 requests per minute
+
+
+//Define middleware function to enforce the rate limit
+const rateLimitMiddleware = (req, res, next) => {
+    const ipAddress = req.ip;
+
+    //Get current request count for the IP address
+    const count = requestLCount.get(ipAddress) || 0;
+
+    //Check if the IP address has exceeded the rate limit
+    if (count >= rateLimit) {
+        return res.status(429).send('To many requests');
+    }
+
+    //Increment the request count for the IP address
+    requestLCount.set(ipAddress, count + 1);
+
+    //Reset the request count after 1 minute
+    setTimeout(() => {
+        requestLCount.delete(ipAddress);
+    }, 60000); //60000 miliseconds = 1 minute
+
+    //Call the next middleware function
+    next();
+};
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/src/index.html');
 });
 
-app.post('/', (req, res) => {
+app.post('/', rateLimitMiddleware, (req, res) => {
     console.log(req.body);
-    res.send("success");
+    
+    try {
+        const email = req.body.email;
+        console.log('poczta: ', email)
+        const emailRegex = new RegExp("^[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\\.)+[a-zA-Z]{2,6}$", "gi");
 
-    const transporter = nodemailer.createTransport({
-        service: '',
-        auth: {
-            user: '',
-            pass: ''
+        let testedEmail = emailRegex.test(email);
+        if (testedEmail == false) {
+            throw new Error('Incorrect email address')
+        } else {
+            if (testedEmail == true) {
+                
+                const transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: 'appTesterAccEmail@gmail.com',
+                        
+                    }
+                })
+            
+                const mailOptions = {
+                    from: req.body.email,
+                    to: 'appTesterAccEmail@gmail.com',
+                    subject: `Message from ${req.body.email}: ${req.body.subject}`,
+                    text: req.body.message
+                }
+            
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.log(error);
+                        res.send('error');
+                    } else {
+                        console.log('Email sent' + info.response);
+                        res.send('success');
+                    }
+                })
+            }
         }
-    })
-
-    const mailOptions = {
-        from: req.body.email,
-        to: 'kowalczy.przemek@wp.pl',
-        subject: `Message from ${req.body.email}: ${req.body.subject}`,
-        text: req.body.message
+        //console.log('testedEmail: ', testedEmail)
+    } catch (error) {
+        console.error(error);
+        res.status(400).send({ error: error.message });
     }
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-            res.send('error');
-        } else {
-            console.log('Email sent' + info.response);
-            res.send('success');
-        }
-    })
+    //res.send("success");
 });
 
 app.listen(PORT, () => {
